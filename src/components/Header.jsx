7d5065b8faf3681d58c2d0cbee9930bc2167d418
AL-Sayed1 <153854740+AL-Sayed1@users.noreply.github.com>
2024-02-29 07:06:13 +0000

    2/29/2024

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
-import { images } from '../constants'
+import React from 'react';
+import { images } from '../constants';
 
 const Header = () => {
   return (
     <section>
-        <Header>
-            <div>
-                <img src={images.Logo} alt='logo' />
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Article</a></li>
-                    <li><a href="">Pages</a></li>
-                    <li><a href="">Pricing</a></li>
-                    <li><a href="">Faq</a></li>
-                </ul>
-            </div>
-            <div>
-            
-            </div>
-        </Header>
+      <header>
+        <div>
+          <img src={images.Logo} alt='logo' />
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Article</a></li>
+            <li><a href="/">Pages</a></li>
+            <li><a href="/">Pricing</a></li>
+            <li><a href="/">Faq</a></li>
+          </ul>
+          <button>sign in</button>
+        </div>
+        <div>
+
+        </div>
+      </header>
     </section>
-  )
-}
+  );
+};
 
 export default Header
 
