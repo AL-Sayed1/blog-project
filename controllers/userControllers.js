3dbe8bf8d37ec2120e1e81c561b6760c5a207e17
AL-Sayed1 <153854740+AL-Sayed1@users.noreply.github.com>
2024-03-30 17:51:02 +0000

    18 - fixing bugs

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -6,7 +6,7 @@ const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
 
-    // check ifs the user exists or not
+    // check if the user exists or not
     let user = await User.findOne({ email });
 
     if (user) {
@@ -123,18 +123,21 @@ const updateProfilePicture = async (req, res, next) => {
 
     upload(req, res, async function (err) {
       if (err) {
-        const error = new Error("An unknown error occured when uploading " + err.message);
+        const error = new Error(
+          "An unknown error occured when uploading " + err.message
+        );
         next(error);
       } else {
         // every thing went well
         if (req.file) {
-          const updatedUser = await User.findByIdAndUpdate(
-            req.user._id,
-            {
-              avatar: req.file.filename,
-            },
-            { new: true }
-          );
+          let filename;
+          let updatedUser = await User.findById(req.user._id);
+          filename = updatedUser.avatar;
+          if (filename) {
+            fileRemover(filename);
+          }
+          updatedUser.avatar = req.file.filename;
+          await updatedUser.save();
           res.json({
             _id: updatedUser._id,
             avatar: updatedUser.avatar,
